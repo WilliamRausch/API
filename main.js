f987efaec8940ce8f55c8e73bd28a31eb4acf096
WilliamRausch <nolanrausch@Gmail.com>
2017-08-07 16:10:52 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,56 @@
+// 1. First, find our UL Container
+let octo = {
+  "login": "octocat",
+  "id": 1,
+  "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/octocat",
+  "html_url": "https://github.com/octocat",
+  "followers_url": "https://api.github.com/users/octocat/followers",
+  "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+  "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+  "organizations_url": "https://api.github.com/users/octocat/orgs",
+  "repos_url": "https://api.github.com/users/octocat/repos",
+  "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/octocat/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "monalisa octocat",
+  "company": "GitHub",
+  "blog": "https://github.com/blog",
+  "location": "San Francisco",
+  "email": "octocat@github.com",
+  "hireable": false,
+  "bio": "There once was...",
+  "public_repos": 2,
+  "public_gists": 1,
+  "followers": 20,
+  "following": 0,
+  "created_at": "2008-01-14T04:33:35Z",
+  "updated_at": "2008-01-14T04:33:35Z"
+}
+
+let myList = document.querySelector('.container');
+console.log(myList);
+ //myList.innerHTML += `<h2> fryman </h2>`;
+
+// 2. Create our Ajax Request
+
+function reqListener(){
+  let data = JSON.parse(this.responseText);
+  //console.log(data.results[6].name);
+  console.log(data);
+
+  for (var i = 0; i < 10; i++) {
+     List.innerHTML+= `<p> shanannanan </p>`;
+  }
+
+}
+
+let request = new XMLHttpRequest();
+
+request.open('GET', 'https://api.github.com/users/WilliamRausch');
+request.addEventListener('load', reqListener);
+request.send();
